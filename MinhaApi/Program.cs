c870fc3778b738de3af4f5a516f6590533ea4df2
Lucas de Oliveira Prado <lucas.oliveira@inovafarma.com.br>
2024-04-17 17:32:31 -0300

    improve: conclusao do modulo introducao e minimal apis

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Ok());
+
+app.MapGet("/name/{nome}", (string nome) => Results.Ok($"Olá {nome}!"));
+
+// app.MapPost("/", (User user) => Results.Ok($"Usuário: {user.Username}| Id: {user.Id}"));
+app.MapPost("/", (User user) =>  Results.Ok(user));
 
 app.Run();
+
+public class User {
+    public int Id { get; set;}
+
+    public string? Username { get; set;}
+}
